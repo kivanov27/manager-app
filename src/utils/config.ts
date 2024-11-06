1e6bad083f52ba367dcbc5c5513f6f122fc413e5
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2024-11-06 10:46:33 +0200

    idk I guess I had something wrong in my config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,6 +3,6 @@ import 'dotenv/config';
 const PORT: string | undefined = process.env.PORT;
 const URL: string | undefined = process.env.NODE_ENV === 'test'
     ? process.env.TEST_URL
-    : process.env.URL
+    : process.env.URL;
 
 export { PORT, URL };
